17ff1604113d8cf0825bdfd41d5b6489dd007876
Muhammad Muhib <m.muhibmehboob@gmail.com>
2022-08-20 17:19:42 +0500

    chap 3 completed random num and guess

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
+Random rValue = Random();
 
 int x = 4;
 
@@ -25,36 +28,67 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           //crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Center(child: Text("Lottery Winning Number is $x")),
+            Center(
+                child: Text(
+              "Lottery Winning Number is 5",
+              style: TextStyle(fontFamily: "Rubik Medium"),
+            )),
+            const SizedBox(
+              height: 20,
+            ),
             Container(
               height: 250,
+              width: 300,
               decoration: BoxDecoration(
                   color: Colors.grey.withOpacity(0.3),
                   borderRadius: BorderRadius.circular(15)),
               child: Padding(
                 padding: EdgeInsets.all(15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.error_outline,
-                      color: Colors.red.shade400,
-                      size: 35,
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Text("Better luck next time.Your number is $x.\nTry again",
-                        textAlign: TextAlign.center),
-                  ],
-                ),
+                child: x == 5
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.done_rounded,
+                            color: Colors.green,
+                            size: 35,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                              "congrats you won the lottery Your number was $x",
+                              textAlign: TextAlign.center),
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.error_outline,
+                            color: Colors.red.shade400,
+                            size: 35,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                              "Better luck next time.Your number is $x.\nTry again",
+                              textAlign: TextAlign.center),
+                        ],
+                      ),
               ),
             )
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            x = rValue.nextInt(6);
+            print(x);
+            setState(() {});
+          },
           child: Icon(Icons.refresh_rounded),
         ),
       ),
