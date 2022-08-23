63ddcfe65b74bb0fcb64180cad3cc24c3fb29788
Muhammad Muhib <m.muhibmehboob@gmail.com>
2022-08-23 20:36:29 +0500

    chap5 navigator done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:tb_course/home_screen.dart';
+import 'package:tb_course/screen_two.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Expanded(
-          child: Container(
-            color: Colors.amber,
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+        ScreenTwo.id: (context) => ScreenTwo(),
+      },
     );
   }
 }
