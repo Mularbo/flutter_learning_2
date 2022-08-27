aec29fda67debd118d9d7ee8c2f532381024cab4
Muhammad Muhib <m.muhibmehboob@gmail.com>
2022-08-27 18:06:59 +0500

    part 8 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tb_course/example_two.dart';
 import 'package:tb_course/home_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: ExampleTwo(),
     );
   }
 }
